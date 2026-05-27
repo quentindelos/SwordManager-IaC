21b9c99603918fd7efcb01a8570d2e46dd97fc39
quentindelos <delosquentin@gmail.com>
2026-05-28 00:06:59 +0200

    add bucket tfstate & edit compute instance to ip static

--- envs/dev/variables.tf
+++ envs/dev/variables.tf
@@ -1,11 +1,27 @@
 variable "project_id" {
-    type = string 
+  type = string
 }
 
-variable "region" { 
-    type = string 
+variable "region" {
+  type = string
 }
 
-variable "domain_name" { 
-    type = string 
+variable "domain_name" {
+  type = string
+}
+
+variable "vpc_network" {
+  type = string
+}
+
+variable "vpc_subnetwork" {
+  type = string
+}
+
+variable "db_private_ip" {
+  type = string
+}
+
+variable "tfstate_bucket_name" {
+  type = string
 }
